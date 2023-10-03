96916ae92e7e08729ebedc899ffb688009544479
binhdoitsme <binhdoitsme@gmail.com>
2023-10-04 00:51:33 +0700

    binh_dh: update analytics backend

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,19 +1,22 @@
+import asyncio
+import json
 import os
 import re
 from datetime import datetime
+from threading import Thread
 from typing import Any
+from fastapi.responses import StreamingResponse
 
-import kafka
 import pydantic
 import uvicorn
-from fastapi import FastAPI, Response, status
+from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
+from fastapi import FastAPI, Response, WebSocket, WebSocketDisconnect, status
 from pydantic.alias_generators import to_camel
 
 app = FastAPI()
 
-producer = kafka.KafkaProducer(
-    bootstrap_servers=[os.getenv("KAFKA_BROKER")]
-)
+loop = asyncio.get_event_loop()
+producer = AIOKafkaProducer(loop=loop, bootstrap_servers=os.getenv("KAFKA_BROKER", ""))
 stream_analytics_topic = "stream_analytics"
 live_report_topic = "live_report"
 
@@ -55,14 +58,85 @@ class ClickTrackingResult(pydantic.BaseModel):
 
 
 @app.post("/")
-def register_click(request: ClickTrackingResult):
+async def register_click(request: ClickTrackingResult):
     value = request.model_dump_json()
-    producer.send(topic=stream_analytics_topic, value=value)
+    await producer.send(topic=stream_analytics_topic, value=value.encode())
+    # await producer.send(topic=live_report_topic, value=value.encode())
+    print("......")
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-def main():
-    producer.send(topic=stream_analytics_topic, value=b"Hello Kafka!")
-    producer.flush()
+class ClickReportRequest(pydantic.BaseModel):
+    ...
 
-    uvicorn.run(app)
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+        self.consumer = AIOKafkaConsumer(
+            live_report_topic,
+            loop=loop,
+            bootstrap_servers=os.getenv("KAFKA_BROKER", ""),
+        )
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+        await websocket.send_text("")
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def consume(self):
+        await self.consumer.start()
+        try:
+            async for msg in self.consumer:
+                print(msg)
+                await self.broadcast(json.loads(str(msg.value)))
+        finally:
+            await self.consumer.stop()
+
+    async def broadcast(self, message: Any):
+        for connection in self.active_connections:
+            await connection.send_json(message)
+
+
+manager = ConnectionManager()
+
+
+@app.post("/reports")
+async def get_reports(request: ClickReportRequest):
+    return {}
+
+
+@app.websocket("/ws/reports")
+async def stream_reports(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            # keep alive
+            _ = await websocket.receive_text()
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+
+
+@app.get("/streaming")
+async def streaming_example():
+    async def generator():
+        for i in range(100):
+            yield f"reload time {i}\n"
+            await asyncio.sleep(1)
+    return StreamingResponse(generator(), media_type="text/event-stream")
+
+
+@app.on_event("startup")
+async def startup_event():
+    await producer.start()
+    loop.create_task(manager.consume())
+
+
+# def main():
+    # producer.send(topic=stream_analytics_topic, value=b"Hello Kafka!")
+    # producer.flush()
+
+    # uvicorn.run(app, host="0.0.0.0")
